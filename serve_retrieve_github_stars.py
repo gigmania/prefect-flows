1f62d2fce48de09762810883976da66293c56cc9
gigmania <tyson.daugherty@gmail.com>
2024-12-08 07:45:34 -0500

    added tasks

--- serve_retrieve_github_stars.py
+++ serve_retrieve_github_stars.py
@@ -1,10 +1,18 @@
-from prefect import flow
+from prefect import flow, task
 from time import sleep
+
+@task(log_prints=True)
+def print_string(string):
+    print(string)
+
+@task(log_prints=True)
+def sleep_num(num):
+    sleep(num)
 
 @flow(log_prints=True)
 def retrieve_github_stars():
-    print("I'm so tired...")
-    sleep(1)
+    print_string("I'm so tired...")
+    sleep_num(1)
     for i in range(10):
-        print("zzzzz...")
-        sleep(6)
+        print_string("zzzzz...")
+        sleep_num(3)
