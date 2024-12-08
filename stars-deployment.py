5573facc31422b264a8cf30d9cc596d44505051e
gigmania <tyson.daugherty@gmail.com>
2024-12-07 21:38:30 -0500

    deply fix

--- stars-deployment.py
+++ stars-deployment.py
@@ -1,9 +1,10 @@
 from prefect import serve
 from serve_retrieve_github_stars import retrieve_github_stars  # import your actual flow
 
-serve(
-    retrieve_github_stars,
-    name="github-stars-deployment",
-    work_pool_name="default-pool",
-    interval="1 minute"  # or cron="0 0 * * *" for daily at midnight
-)
+if __name__ == "__main__":
+  serve(
+      retrieve_github_stars,
+      name="github-stars-deployment",
+      pool_name="default-pool",
+      interval="1 minute"  # or cron="0 0 * * *" for daily at midnight
+  )
