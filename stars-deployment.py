ae97feeec34c47da09e92455133bd749bf9e32da
gigmania <tyson.daugherty@gmail.com>
2024-12-07 21:40:00 -0500

    deploy not serve

--- stars-deployment.py
+++ stars-deployment.py
@@ -1,10 +1,10 @@
-from prefect import serve
+from prefect import serve, deploy
 from serve_retrieve_github_stars import retrieve_github_stars  # import your actual flow
 
 if __name__ == "__main__":
-  serve(
-      retrieve_github_stars,
-      name="github-stars-deployment",
-      pool_name="default-pool",
-      interval="1 minute"  # or cron="0 0 * * *" for daily at midnight
-  )
+    deployment = deploy(
+        retrieve_github_stars,
+        name="github-stars-deployment",
+        pool="default-pool",
+        interval="1 minute"
+    )
